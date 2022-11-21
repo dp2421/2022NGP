dbb91ad809a585de4ab8385563bbe065457419e3
Son Meenki <meenki98@gmail.com>
2022-11-22 00:04:10 +0900

    Timer Class 작성

--- NGPTermProjectServer/NGPTermProjectServer/Timer.h
+++ NGPTermProjectServer/NGPTermProjectServer/Timer.h
@@ -1,6 +1,20 @@
 #pragma once
+#include "stdafx.h"
 
 class Timer
 {
-};
+public:
+	Timer() {}
+	~Timer() {}
 
+	void Begin()
+	{
+		begin = chrono::high_resolution_clock::now();	
+	}
+
+	double GetDelteTime() { return GetDeltaTimeMilli() * 1e-03; }
+	int GetDeltaTimeMilli() { return (chrono::high_resolution_clock::now() - begin).count(); }
+
+private:
+	chrono::steady_clock::time_point begin = chrono::high_resolution_clock::now();
+};
